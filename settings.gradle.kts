e88fa1c7a31670e9c59ae71c268a45bc329a1446
saadsaud003 <57197218+saadsaud003@users.noreply.github.com>
2026-03-26 05:16:20 +0300

    Create settings.gradle.kts with gradle plugin management and dependency resolution configurations

---
+++ settings.gradle.kts
@@ -0,0 +1,13 @@
+pluginManagement { 
+    repositories { 
+        gradlePluginPortal() 
+        mavenCentral() 
+    } 
+}
+
+dependencyResolutionManagement { 
+    repositories { 
+        mavenCentral() 
+        google() 
+    } 
+}
